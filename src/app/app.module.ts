b73be660c42b7fe1d3e10d104d56f597405914ff
CORP\hfenyom <herve.toukam12@gmail.com>
2020-12-03 10:52:58 +0100

    Resolve bug in 1 - Architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
-import { StartComponent } from './start/start.component';
-import { BoxComponent } from './box/box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    StartComponent,
-    BoxComponent
   ],
   imports: [
     BrowserModule,
